b8d818d2f8efcf79e5cd85d07566b04e134d818d
Kristof De Kimpe <kristofdk@users.noreply.github.com>
2015-01-31 13:57:42 +0100

    implemented download of stream

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var http = require('follow-redirects').http;
 var url = require('url');
 var fs = require('fs');
+var path = require('path');
 var jsonPath = require('JSONPath');
+
+const downloadDir = 'C:\\Temp\\Download';
 
 function findStreamUrl(collectionId, cb) {
 	var requestOptions = {
@@ -29,31 +32,29 @@ function findStreamUrl(collectionId, cb) {
 	});
 }
 
-/* Test for Duyster */
-findStreamUrl('b46955dd-91ef-11e3-b45a-00163edf75b7', function(streamUrl) {
+function downloadStream(streamUrl) {
+	var filename = streamUrl.substr(streamUrl.lastIndexOf('/') + 1);
+
 	var parsedStreamUrl = url.parse(streamUrl);
 
-	var fileWriteStream = fs.createWriteStream('C:\\Temp\\duyster.mp3', {})
+	var fileWriteStream = fs.createWriteStream(path.join(downloadDir, filename), {});
 	fileWriteStream.on('error', function(e) {
 		console.log("Error when writing to file: " + e.message);
 	});
 
 	var options = {
 		hostname: parsedStreamUrl.hostname,
-		port: parsedStreamUrl.port,
+		port: 80,
 		path: parsedStreamUrl.path,
 		method: 'GET'
 	};
 
 	var req = http.request(options, function(res) {
-		console.log('STATUS: ' + res.statusCode);
-		console.log('HEADERS: ' + JSON.stringify(res.headers));
-		
 		res.on('data', function (chunk) {
 			var successfulWrite = fileWriteStream.write(chunk);
-			console.log(successfulWrite);
 		});
 	});
+	req.setTimeout(5000);
 	req.on('end', function() {
 		req.end();
 		fileWriteStream.end();
@@ -61,4 +62,15 @@ findStreamUrl('b46955dd-91ef-11e3-b45a-00163edf75b7', function(streamUrl) {
 	req.on('error', function(e) {
 		console.log('Error when fetching stream: ' + e.message);
 	});
+	req.end();
+}
+
+[
+	'b46955dd-91ef-11e3-b45a-00163edf75b7', // duyster
+	'e2889321-90d2-11e3-b45a-00163edf75b7', // de afrekening
+	'76d642c3-91c7-11e4-bdf6-00163edf75b7', // de hotlist
+].forEach(function (collectionId) {
+	findStreamUrl(collectionId, function(streamUrl) {
+		downloadStream(streamUrl);
+	})
 });
